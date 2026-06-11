f9f89d08b326e87d07562609242088a4adb480c2
Lucie BARREZ <lucie@Lucies-MacBook-Air.local>
2026-06-11 15:03:56 +0200

    fix: ajout de la fonction get_db

--- app/database.py
+++ app/database.py
@@ -6,3 +6,11 @@ SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@localhost:5432/ymmo_db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+# Fonction pour obtenir une session de base de données par requête
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
